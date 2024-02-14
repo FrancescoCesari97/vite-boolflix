ea06e040388f55acea8597975f7cbd3d4a09d16a
FrancescoCesari97 <freedus97@gmail.com>
2024-02-14 15:35:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+
+
+<script >
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default{
+  data(){
+    return{
+      title: 'Boolflix',
+    };
+  },
+
+  components: {AppHeader, AppMain},
+};
+</script>
+
+<template>
+  <app-header> </app-header>
+
+  <app-main> </app-main>
+
+</template>
+
+<style lang="scss">
+
+@use './styles/general.scss'
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script >
+
+
+</script>
+
+<template>
+
+   <div class="container">
+    <div class="input-group flex-nowrap search-bar">
+    <input type="text" class="form-control" placeholder="movie" aria-label="Username" aria-describedby="addon-wrapping">
+</div>
+
+   </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+.container{
+
+    height: 80px;
+    
+    .search-bar{
+        margin-top: 20px;
+    }
+}
+
+
+
+</style>
